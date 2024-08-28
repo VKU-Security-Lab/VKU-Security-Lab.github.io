8469e446361465c132ff7377c90d24073e66e2fd
Shr3wd <tripm2112@gmail.com>
2024-08-28 16:31:29 +0700

    Update avatar

--- data/member-data.js
+++ data/member-data.js
@@ -6,7 +6,7 @@ export default function memberList() {
             year: [2023, 2024],
             role: ["co-founder", "web", "forensic"],
             description: "Sống là để chiến đấu",
-            imgSrc: "dragon0zz.jpg",
+            imgSrc: "dragon0zz.png",
             github: "https://github.com/lelong1210",
             mail: "lequanglong12102k2@gmail.com"
         },
