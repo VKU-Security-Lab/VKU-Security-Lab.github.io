408128a7b03fcff81fd69ef3268019cf1328a35c
Shr3wd <tripm2112@gmail.com>
2024-08-28 07:31:19 +0700

    Update writeup-data.js

--- data/writeup-data.js
+++ data/writeup-data.js
@@ -11,15 +11,15 @@ export default function writeupList() {
             author: "Shr3wd",
         },
     
-        {
-            id: 2,
-            title: "(Writeup) Digital Dragon CTF 2024 - Quals",
-            date: "26/08/2024",
-            description: "This is the first writeup This is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeup",
-            link: "digital-dragon-ctf-2024-warmup.md",
-            tags: ["web", "re"],
-            banner: "ddc.png",
-            author: "Shr3wd",
-        },
+        // {
+        //     id: 2,
+        //     title: "(Writeup) Digital Dragon CTF 2024 - Quals",
+        //     date: "26/08/2024",
+        //     description: "This is the first writeup This is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeupThis is the first writeup",
+        //     link: "digital-dragon-ctf-2024-warmup.md",
+        //     tags: ["web", "re"],
+        //     banner: "ddc.png",
+        //     author: "Shr3wd",
+        // },
     ];
 }
