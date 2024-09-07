cd29e0ecf73dba0346aa43dc9eceb1f34de020b0
Shr3wd <tripm2112@gmail.com>
2024-09-07 21:39:04 +0700

    Update

--- page/elements/header.js
+++ page/elements/header.js
@@ -21,7 +21,7 @@ export default function Header() {
     <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
     <div class="header-container">
         <a class="navbar-brand" href="/">
-            <img src="/images/logo.png" alt="VSL - VKU Security Lab" id="logo-header" width="30" height="30" class="d-inline-block align-text-top">
+            <img src="/images/logo.jpg" alt="VSL - VKU Security Lab" id="logo-header" width="30" height="30" class="d-inline-block align-text-top">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
